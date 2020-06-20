c1f9fa702d3a63a4a0fb9e7f0bac69429ae24f33
Melissa Cadena <melissabcadena@Melissas-MacBook-Pro.local>
2020-06-19 23:42:46 -0500

    create click function tochange p to textarea on click to edit tasks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // show current date at top of page
-
-//get current date and save to variable 
 var currentDate = moment().format("dddd, MMMM Do");
-console.log(currentDate);
 $("#currentDay").text(currentDate);
+
+// create event listener for click on task item
+$(".task-info").on("click", "p",  function () {
+    // once clicked, get current text within p, if nothing make it an empty string
+    var text = $(this).text().trim() || "";
+    // create <textarea> element
+    var textArea = $("<text-area>").addClass("form-control").text(text);
+    console.log(textArea.text());
+    // replace p w/ text area
+    $(this).replaceWith(textArea);
+    textArea.trigger("focus");
+})
